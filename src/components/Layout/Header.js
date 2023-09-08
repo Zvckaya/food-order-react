77b7b927990590abf45ee29047050fddd0794b48
lzvckaya <lzvckaya@gmail.com>
2023-09-08 23:56:10 +0900

    오타 수정

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
@@ -7,11 +9,10 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="맛있는 식탁"></img>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
